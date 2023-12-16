26c0269a54ef82867bff8040c92fccadf0cef4de
Kober <pedrokoberstain2@gmail.com>
2023-12-15 21:18:29 -0300

    correção do exercicio 7

--- exercicios/estutura-condicional/src/Ex07.java
+++ exercicios/estutura-condicional/src/Ex07.java
@@ -5,7 +5,6 @@ public class Ex07 {
 
     public static void main(String[] args) {
 
-        public static void main(String[] args) {
             Locale.setDefault(Locale.US);
             Scanner sc = new Scanner(System.in);
 
