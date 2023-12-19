ff17f185cde22f5795debe1ac696ba2266ee3937
Kober <pedrokoberstain2@gmail.com>
2023-12-19 17:52:24 -0300

    novos exercicios

---
+++ exercicios-3semana/OrderStatus/src/entities/Order.java
@@ -0,0 +1,27 @@
+package entities;
+
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+    private Date moment;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(Integer id, Date moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
